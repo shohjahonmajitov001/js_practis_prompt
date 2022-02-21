790fbbdf9d5895c696c7ba38955fde134716fa40
Shohjahon <shohjahonmajitov001@gmail.com>
2022-02-21 15:08:42 +0500

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var firstName = prompt('Ismingizni kiriting');
+var firstQuestion = prompt('Bootstrapning eng oxirgi versiyasi?');
+var secondQuestion = prompt('Bootstrapni qaysi kampaniya ishchilari ishlab chiqqan?');
+var thirdQuestion = prompt('Bootstrapga oxshash yana bir fremwork nomi?');
+
+console.log(firstName + '\n' + firstQuestion + '\n'
++ secondQuestion + '\n' + thirdQuestion);
